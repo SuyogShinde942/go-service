dd4a47287c68aa9340e1b3c13be6d3a3ca1e236b
Suyog Shinde <omshinde942@gmail.com>
2022-12-19 10:28:59 +0530

    created a dockerfile for the go-service

---
+++ main.go
@@ -0,0 +1,15 @@
+package main;
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+func main() {
+    http.HandleFunc("/helloworld", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hello, World! Thank YOU APPLIEDAI FOR THE TASK")
+    })
+    fmt.Printf("Server running (port=8080), route: http://localhost:8080/helloworld\n")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
